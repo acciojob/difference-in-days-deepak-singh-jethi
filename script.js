d0bcd68330ab158f357be3a5eef9085f27f4bb53
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-19 18:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+   // Parse the input strings into Date objects
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+
+    // Calculate the difference in milliseconds
+    const timeDiff = Math.abs(d2 - d1);
+
+    // Convert the difference from milliseconds to days
+    const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+    // Determine the sign based on the order of dates
+    return (d2 > d1) ? daysDiff : -daysDiff;
 };
 
 // Do not change the code below.
